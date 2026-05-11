15d75af0b2e204d3c0d889007ceaeb9929c92b03
Tomas Cokis <contact@tomascokis.com>
2026-05-12 00:28:48 +1000

    fixed paths

--- docker/start.sh
+++ docker/start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-mkdir -p /site
+cd /salsasite
 
 echo "Building salsa site..."
-Rscript /project/build.R || echo "Initial build failed"
+Rscript /salsasite/build.R || echo "Initial build failed"
 
 echo "Starting nginx..."
 nginx
 
 while true; do
   echo "Rebuilding salsa site..."
-  Rscript /project/build.R || echo "Build failed"
+  cd /salsasite
+  Rscript /salsasite/build.R || echo "Build failed"
   sleep 3600
 done
